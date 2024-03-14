9a07b744a3514678a9ce9281f9c24b170e36ad7f
Richard Schneider <richard@schneiderbox.net>
2024-03-13 21:07:05 -0500

    Implement state string conversions

--- engine/alonso.c
+++ engine/alonso.c
@@ -4,9 +4,17 @@
 
 
 int main() {
-    //uint64_t s = 0b0000000000000100011101010000010000000000011000100000100000001000;
+    uint64_t s = 0b0000000000000100011101010000010000000000011000100000100000001000;
 
     uint64_t children[MAX_CHILDREN];
     int childrenc = state_children(0, children);
     printf("%d\n", childrenc);
+
+    state_draw(s);
+
+    char state_string[STATE_STRING_SIZE];
+    state_to_string(s, state_string);
+    printf("%s\n", state_string);
+
+    state_draw(state_from_string(state_string));
 }
--- engine/state.c
+++ engine/state.c
@@ -11,10 +11,10 @@ void state_draw(uint64_t state) {
             int tilei = i*5 + j;
 
             char c = '.';
-            if (((1llu << tilei) & state) > 0) {
+            if ((1llu << tilei) & state) {
                 c = 'X';
             }
-            else if (((1llu << (tilei + 32)) & state) > 0) {
+            else if ((1llu << (tilei + 32)) & state) {
                 c = 'O';
             }
             fputc(c, stderr);
@@ -22,6 +22,36 @@ void state_draw(uint64_t state) {
         fputc('\n', stderr);
     }
 };
+
+
+void state_to_string(uint64_t state, char state_string[]) {
+    for (int i = 0; i < 25; i++) {
+        if ((1llu << i) & state) {
+            state_string[i] = 'x';
+        } else if ((1llu << (i + 32)) & state) {
+            state_string[i] = 'o';
+        } else {
+            state_string[i] = '.';
+        }
+    }
+    state_string[25] = '\0';
+}
+
+
+uint64_t state_from_string(const char state_string[]) {
+    uint64_t state = 0;
+    for (int i = 0; i < 25; i++) {
+        switch(state_string[i]) {
+            case 'x':
+                state |= (1llu << i);
+                break;
+            case 'o':
+                state |= (1llu << (i + 32));
+                break;
+        }
+    }
+    return state;
+}
 
 
 uint64_t state_swap_players(uint64_t state) {
